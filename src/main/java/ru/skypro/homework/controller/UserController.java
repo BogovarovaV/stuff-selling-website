01a147d7a0efa6a738175b453de6a42cf424a11c
BogovarovaV <v.bogovarova@gmail.com>
2022-07-28 21:17:08 +0500

    Добавлено документирование в контроллеры.

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -1,5 +1,6 @@
 package ru.skypro.homework.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.skypro.homework.dto.NewPasswordDto;
@@ -12,26 +13,46 @@ import java.util.Collection;
 @RequestMapping("/users")
 public class UserController {
 
+    @Operation(
+            tags = "Пользователи (UserController)",
+            summary = "Добавление пользователя (addUser)"
+    )
     @PostMapping
     public ResponseEntity<User> addUser(@RequestBody User user) {
         return ResponseEntity.ok(user);
     }
 
+    @Operation(
+            tags = "Пользователи (UserController)",
+            summary = "Получение пользователей (getUsers)"
+    )
     @GetMapping("/me")
     public ResponseEntity<Collection<UserDto>> getUsers() {
         return ResponseEntity.ok(null);
     }
 
+    @Operation(
+            tags = "Пользователи (UserController)",
+            summary = "Редактирование пользователя (updateUser)"
+    )
     @PatchMapping("/me")
     public ResponseEntity<UserDto> updateUser(@RequestBody UserDto user) {
         return ResponseEntity.ok(user);
     }
 
+    @Operation(
+            tags = "Пользователи (UserController)",
+            summary = "Изменение пароля (setPassword)"
+    )
     @PostMapping("set_password")
     public ResponseEntity<NewPasswordDto> setPassword(@RequestBody NewPasswordDto newPassword) {
         return ResponseEntity.ok(newPassword);
     }
 
+    @Operation(
+            tags = "Пользователи (UserController)",
+            summary = "Получение пользователя по id (getUser)"
+    )
     @GetMapping("{id}")
     public ResponseEntity<UserDto> getUser(@PathVariable("id") int id) {
         return ResponseEntity.ok(new UserDto());
