3af22b2819efc92b605a7979e74a62c0be6860e8
Диана <diana_yudina@mail.ru>
2022-09-02 03:34:50 +0500

    Добавлены рабочие тесты в AdsControllerTest. Пока запускаются 10 из 13.
    Переименованы названия тестов в UserControllerTest.
    Немного скорректированы названия констант в DataTest.

--- src/test/java/ru/skypro/homework/controllerTests/AdsControllerTest.java
+++ src/test/java/ru/skypro/homework/controllerTests/AdsControllerTest.java
@@ -1,7 +1,6 @@
 package ru.skypro.homework.controllerTests;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -15,7 +14,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import ru.skypro.homework.controller.AdsController;
@@ -24,6 +22,7 @@ import ru.skypro.homework.mapper.AdsMapper;
 import ru.skypro.homework.mapper.CommentMapper;
 import ru.skypro.homework.model.AdsAvatar;
 import ru.skypro.homework.model.Advert;
+import ru.skypro.homework.model.Comment;
 import ru.skypro.homework.model.User;
 import ru.skypro.homework.repository.AdsAvatarRepository;
 import ru.skypro.homework.repository.AdvertRepository;
@@ -33,13 +32,17 @@ import ru.skypro.homework.service.AdsAvatarService;
 import ru.skypro.homework.service.impl.AdvertServiceImpl;
 import ru.skypro.homework.service.impl.CommentServiceImpl;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static ru.skypro.homework.DataTest.*;
+
+import static org.mockito.Mockito.*;
 
 @WebMvcTest(controllers = AdsController.class)
 public class AdsControllerTest {
@@ -47,15 +50,23 @@ public class AdsControllerTest {
     private AdsTo adsTo;
     private Advert advert;
     private CreateAdsTo createAdsTo;
+    private FullAdsTo fullAds;
     private UserTo userTo;
     private User user;
     private AdsAvatar adsAvatar;
-    private JSONObject advertObject1;
-    private JSONObject properties;
-    MockMultipartFile file;
+    private JSONObject advertObject;
+    private JSONObject commentObject;
+    private MockMultipartFile adsJson;
+    private MockMultipartFile image;
+    private Comment comment;
+    private AdsCommentTo commentTo;
+    private List<AdsCommentTo> commentToList;
+    private List<AdsTo> adsToList;
+    private List<Advert> advertList;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
+
     private MockMvc mockMvc;
 
     @MockBean
@@ -84,56 +95,77 @@ public class AdsControllerTest {
     AdsController adsController;
 
     @BeforeEach
-    public void setUp() throws JSONException {
+    public void setUp() {
 
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .alwaysDo(MockMvcResultHandlers.print())
-                .build();
-
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 
         //dto
         adsTo = new AdsTo();
         adsTo.setPk(ADS_ID);
-        adsTo.setPrice(PRICE_1);
+        adsTo.setPrice(PRICE);
         adsTo.setTitle(TITLE);
         adsTo.setImage(IMAGE);
         adsTo.setAuthor(USER_ID);
         adsTo.setDescription(DESC);
 
+        adsToList = new ArrayList<>();
+        adsToList.add(adsTo);
+
         userTo = new UserTo();
         userTo.setId(USER_ID);
         userTo.setFirstName(FIRSTNAME);
         userTo.setLastName(LASTNAME);
-        userTo.setEmail(EMAIL_1);
+        userTo.setEmail(EMAIL);
         userTo.setPhone(PHONE);
 
         createAdsTo = new CreateAdsTo();
         createAdsTo.setDescription(DESC);
-        createAdsTo.setPrice(PRICE_1);
+        createAdsTo.setPrice(PRICE);
         createAdsTo.setTitle(TITLE);
 
-        FullAdsTo fullAds = new FullAdsTo();
+        fullAds = new FullAdsTo();
         fullAds.setAuthorFirstName(FIRSTNAME);
         fullAds.setAuthorLastName(LASTNAME);
         fullAds.setDescription(DESC);
-        fullAds.setEmail(EMAIL_1);
+        fullAds.setEmail(EMAIL);
         fullAds.setImage(IMAGE);
         fullAds.setPhone(PHONE);
         fullAds.setPk(ADS_ID);
-        fullAds.setPrice(PRICE_1);
+        fullAds.setPrice(PRICE);
         fullAds.setTitle(TITLE);
 
+        commentTo = new AdsCommentTo();
+        commentTo.setAuthor(USER_ID);
+        commentTo.setCreatedAt(DATE_TIME);
+        commentTo.setPk(COMMENT_ID);
+        commentTo.setText(TEXT_1);
+
+        commentToList = new ArrayList<>();
+        commentToList.add(commentTo);
+
         //entity
-        Advert advert = new Advert();
+
+        advert = new Advert();
         advert.setId(ADS_ID);
-        advert.setPrice(PRICE_1);
+        advert.setPrice(PRICE);
         advert.setTitle(TITLE);
         advert.setImage(IMAGE);
         advert.setDescription(DESC);
-        user = new User();
-        user.setId(USER_ID);
-        advert.setUser(user);
 
+        advertList = new ArrayList<>();
+        advertList.add(advert);
+
+        comment = new Comment();
+        comment.setId(COMMENT_ID);
+        comment.setText(TEXT_1);
+        comment.setAds(advert);
+        comment.setCreatedAt(DATE_TIME);
+
+        user = new User(USER_ID, FIRSTNAME, LASTNAME, EMAIL,
+                PHONE, USERNAME, PASSWORD, true, List.of(advert), List.of(comment));
+
+        advert.setUser(user);
+        comment.setUser(user);
 
         adsAvatar = new AdsAvatar();
         adsAvatar.setId(ADS_AVATAR_ID);
@@ -141,40 +173,37 @@ public class AdsControllerTest {
 
         //JSON
 
-        file = new MockMultipartFile(
-                "file",
+        adsJson = new MockMultipartFile(
+                "properties",
+                "",
+                "application/json",
+                "{\"description\": \"description1\", \"price\": \"10000\",\"title\": \"title1\"}".getBytes());
+
+        image = new MockMultipartFile(
                 "image",
-                MediaType.MULTIPART_FORM_DATA_VALUE, ADS_AVATAR_IMAGE);
+                "image.png",
+                MediaType.IMAGE_PNG_VALUE, ADS_AVATAR_IMAGE);
 
-        properties = new JSONObject();
-        properties.put("descripton", DESC);
-        properties.put("price", PRICE_1);
-        properties.put("title", TITLE);
-        advertObject1 = new JSONObject();
-        advertObject1.put("properties", properties);
-        advertObject1.put("file", file);
+        advertObject = new JSONObject();
+        advertObject.put("author", USER_ID);
+        advertObject.put("image", IMAGE);
+        advertObject.put("pk", ADS_ID);
+        advertObject.put("price", PRICE_2);
+        advertObject.put("title", TITLE);
+        advertObject.put("description", DESC);
 
-
-
-
-
-
-
-//        ResponseWrapperAdsTo responseWrapperAds = new ResponseWrapperAdsTo();
-//        responseWrapperAds.setCount(COUNT);
-//        responseWrapperAds.setResults(List.of(adsTo));
-//
-//        RegReqTo regReqTo = new RegReqTo();
-//        regReqTo.setPassword(PASSWORD);
-//        regReqTo.setUsername(USERNAME);
-//        regReqTo.setRole(RegReqTo.RoleEnum.ADMIN);
+        commentObject = new JSONObject();
+        commentObject.put("author", USER_ID);
+        commentObject.put("createdAt", DATE_TIME.toString());
+        commentObject.put("pk", COMMENT_ID);
+        commentObject.put("text", TEXT_2);
 
     }
 
 
     @WithMockUser(username = "admin", authorities = "ADMIN")
     @Test
-    public void testGetAllAds() throws Exception {
+    public void testShouldGetAllAds() throws Exception {
         when(adsMapper.advertEntitiesToAdsDtos(any(List.class))).thenReturn((List.of(adsTo)));
 
         mockMvc.perform(MockMvcRequestBuilders
@@ -185,81 +214,211 @@ public class AdsControllerTest {
                 .andExpect(jsonPath("$.results[*].author").value(USER_ID))
                 .andExpect(jsonPath("$.results[*].image").value(IMAGE))
                 .andExpect(jsonPath("$.results[*].pk").value(ADS_ID))
-                .andExpect(jsonPath("$.results[*].price").value(PRICE_1))
+                .andExpect(jsonPath("$.results[*].price").value(PRICE))
                 .andExpect(jsonPath("$.results[*].title").value(TITLE))
                 .andExpect(jsonPath("$.results[*].description").value(DESC));
     }
 
-    @WithMockUser(username = "admin", authorities = "ADMIN")
+    @WithMockUser(username = USERNAME, authorities = "USER")
     @Test
-    public void testCreateAds() throws Exception {
-        when(adsMapper.createAdsDtoToAdvertEntity(createAdsTo)).thenReturn(advert);
+    public void testShouldCreateAds() throws Exception {
+        when(auth.getName()).thenReturn(USERNAME);
+        when(adsMapper.createAdsDtoToAdvertEntity(any())).thenReturn(advert);
         when(userRepository.findUsersByUsername(any())).thenReturn(Optional.ofNullable(user));
-        when(adsAvatarService.saveAds(file)).thenReturn("1");
-        when(adsAvatarRepository.saveAndFlush(adsAvatar)).thenReturn(adsAvatar);
-        when(advertRepository.save(any())).thenReturn(advert);
-
-        mockMvc.perform(multipart("/api/1/image").file(file))
-                .andExpect(status().isOk());
+        when(adsMapper.advertEntityToAdsDto(any())).thenReturn(adsTo);
 
         mockMvc.perform(MockMvcRequestBuilders
-                        .post("/ads")
-                        .content(String.valueOf(advertObject1))
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
+                        .multipart("/ads")
+                        .file(adsJson)
+                        .file(image))
+
+                .andExpect(status().isOk());
+    }
+
+//    @Test
+//    public void testShouldThrowException_whenUnauthorizedUserTryingToCreateAds() throws Exception {
+//        when(adsMapper.createAdsDtoToAdvertEntity(any())).thenReturn(advert);
+//        when(auth.getName()).thenReturn(null);
+//        when(userRepository.findUsersByUsername(any())).thenReturn(Optional.ofNullable(user));
+//        when(adsMapper.advertEntityToAdsDto(any())).thenReturn(adsTo);
+//
+//        mockMvc.perform(MockMvcRequestBuilders
+//                        .multipart("/ads")
+//                        .file(adsJson)
+//                        .file(image))
+//
+//                .andExpect(status().isForbidden());
+//    }
+
+
+//    @WithMockUser(username = USERNAME, password = PASSWORD, authorities = "USER")
+//    @Test
+//    public void testShouldGetAdsMe() throws Exception {
+//        when(auth.getName()).thenReturn(EMAIL);
+//        when(userRepository.findUsersByUsername(any())).thenReturn(Optional.ofNullable(user));
+//        when(adsMapper.advertEntitiesToAdsDtos(any())).thenReturn(adsToList);
+//
+//        mockMvc.perform(MockMvcRequestBuilders
+//                        .get("/ads/me")
+//                )
+//                .andExpect(status().isOk());
+//
+//    }
+
+    @WithMockUser(username = "user", authorities = "USER")
+    @Test
+    public void testShouldFindOneAdsBySearchOrAll() throws Exception {
+        when(adsMapper.advertEntitiesToAdsDtos(any())).thenReturn(adsToList);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/ads")
+                )
+                .andExpect(status().isOk());
+
+    }
+
+    @WithMockUser(username = USERNAME, password = PASSWORD, authorities = "USER")
+    @Test
+    public void testShouldRemoveAds() throws Exception {
+        when(auth.getName()).thenReturn(USERNAME);
+        when(advertRepository.findById(any())).thenReturn(Optional.ofNullable(advert));
+        doNothing().when(adsAvatarRepository).delete(any());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/ads/"+ USER_ID)
                 )
                 .andExpect(status().isOk());
     }
 
-//
+    @WithMockUser(username = USERNAME, password = PASSWORD, authorities = "USER")
+    @Test
+    public void testShouldGetAdsById() throws Exception {
+        when(auth.getName()).thenReturn(USERNAME);
+        when(advertRepository.findById(any())).thenReturn(Optional.ofNullable(advert));
+        when(adsMapper.advertEntityToFullAdsDto(any())).thenReturn(fullAds);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/ads/" + ADS_ID)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.authorFirstName").value(FIRSTNAME))
+                .andExpect(jsonPath("$.authorLastName").value(LASTNAME))
+                .andExpect(jsonPath("$.description").value(DESC))
+                .andExpect(jsonPath("$.email").value(EMAIL))
+                .andExpect(jsonPath("$.phone").value(PHONE))
+                .andExpect(jsonPath("$.pk").value(ADS_ID))
+                .andExpect(jsonPath("$.price").value(PRICE))
+                .andExpect(jsonPath("$.title").value(TITLE));
+    }
+
+//    @WithMockUser(username = USERNAME, password = PASSWORD, authorities = "USER")
 //    @Test
-//    public void testGetAdsMe() {
+//    public void testShouldUpdateAdsById() throws Exception {
+//        when(auth.getName()).thenReturn(USERNAME);
+//        when(auth.getPrincipal()).thenReturn(user);
+//        when(advertRepository.findById(any())).thenReturn(Optional.ofNullable(advert));
+//        advert.setPrice(PRICE_2);
+//        when(adsAvatarService.saveAds(any())).thenReturn(IMAGE);
 //
+//        mockMvc.perform(MockMvcRequestBuilders
+//                        .patch("/ads/" + ADS_ID)
+//                        .content(advertObject.toString())
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON)
+//                )
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.author").value(USER_ID))
+//                .andExpect(jsonPath("$.image").value(IMAGE))
+//                .andExpect(jsonPath("$.pk").value(ADS_ID))
+//                .andExpect(jsonPath("$.price").value(PRICE_2))
+//                .andExpect(jsonPath("$.title").value(TITLE))
+//                .andExpect(jsonPath("$.description").value(DESC));
 //    }
-//
-//    @Test
-//    public void testFindAds() {
-//
-//    }
-//
-//    @Test
-//    public void testRemoveAds() {
-//
-//    }
-//
-//    @Test
-//    public void testGetAds() {
-//
-//    }
-//
-//    @Test
-//    public void testUpdateAds() {
-//
-//    }
-//
-//    @Test
-//    public void testGetAdsComments() {
-//
-//    }
-//
-//    @Test
-//    public void testAddAdsComment() {
-//
-//    }
-//
-//    @Test
-//    public void testDeleteAdsComment() {
-//
-//    }
-//
-//    @Test
-//    public void testGetAdsComment() {
-//
-//    }
-//
-//    @Test
-//    public void testUpdateAdsComment() {
-//
-//    }
+
+    @WithMockUser(username = "user", authorities = "USER")
+    @Test
+    public void testShouldGetAdsComments() throws Exception {
+        when(commentMapper.commentEntitiesToAdsCommentDtos(any())).thenReturn(commentToList);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/ads/" + ADS_ID + "/comments")
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.count").value(COUNT))
+                .andExpect(jsonPath(("$.results")).hasJsonPath())
+                .andExpect(jsonPath("$.results[*].author").value(USER_ID))
+                .andExpect(jsonPath("$.results[*].pk").value(COMMENT_ID))
+                .andExpect(jsonPath("$.results[*].text").value(TEXT_1));
+    }
+
+    @WithMockUser(username = USERNAME, password = PASSWORD, authorities = "USER")
+    @Test
+    public void testShouldAddAdsComments() throws Exception {
+        when(auth.getName()).thenReturn(USERNAME);
+        when(commentMapper.adsCommentDtoToCommentEntity(any())).thenReturn(comment);
+        when(userRepository.findById(any())).thenReturn(Optional.ofNullable(user));
+        when(advertRepository.findById(any())).thenReturn(Optional.ofNullable(advert));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/ads/" + ADS_ID + "/comments")
+                        .content(commentObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.author").value(USER_ID));
+    }
+
+    @WithMockUser(username = USERNAME, password = PASSWORD, authorities = "USER")
+    @Test
+    public void testShouldDeleteAdsComment() throws Exception {
+        when(auth.getName()).thenReturn(USERNAME);
+        when(commentRepository.findCommentById(any())).thenReturn(Optional.of(comment));
+        when(advertRepository.findById(any())).thenReturn(Optional.ofNullable(advert));
+        when(userRepository.getById(any())).thenReturn(user);
+        doNothing().when(commentRepository).delete(any());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/ads/" + ADS_ID + "/comments/" + COMMENT_ID)
+                )
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(username = "user", authorities = "USER")
+    @Test
+    public void testShouldGetAdsCommentsById() throws Exception {
+        when(commentRepository.findCommentById(any())).thenReturn(Optional.of(comment));
+        when(commentMapper.commentEntityToAdsCommentDto(any())).thenReturn(commentTo);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/ads/" + ADS_ID + "/comments/" + COMMENT_ID)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.author").value(USER_ID))
+                .andExpect(jsonPath("$.pk").value(COMMENT_ID))
+                .andExpect(jsonPath("$.text").value(TEXT_1));
+    }
+
+    @WithMockUser(username = USERNAME, authorities = "USER")
+    @Test
+    public void testShouldUpdateAdsCommentsById() throws Exception {
+        when(auth.getName()).thenReturn(USERNAME);
+        when(commentRepository.findCommentById(any())).thenReturn(Optional.ofNullable(comment));
+        comment.setText(TEXT_2);
+        when(advertRepository.findById(any())).thenReturn(Optional.ofNullable(advert));
+        when(userRepository.getById(any())).thenReturn(user);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .patch("/ads/" + ADS_ID + "/comments/" + COMMENT_ID)
+                        .content(commentObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.author").value(USER_ID))
+                .andExpect(jsonPath("$.pk").value(COMMENT_ID))
+                .andExpect(jsonPath("$.text").value(TEXT_2));
+    }
 
 }
--- src/test/java/ru/skypro/homework/controllerTests/UserControllerTest.java
+++ src/test/java/ru/skypro/homework/controllerTests/UserControllerTest.java
@@ -1,5 +1,6 @@
-package ru.skypro.homework.controller;
+package ru.skypro.homework.controllerTests;
 
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -7,10 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import ru.skypro.homework.controller.UserController;
 import ru.skypro.homework.dto.UserTo;
 import ru.skypro.homework.mapper.UserMapper;
 import ru.skypro.homework.model.User;
@@ -33,8 +38,11 @@ class UserControllerTest {
 
     private User user;
     private UserTo userTo;
+    private JSONObject userObject;
 
     @Autowired
+    private WebApplicationContext webApplicationContext;
+
     private MockMvc mockMvc;
 
     @MockBean
@@ -66,13 +74,16 @@ class UserControllerTest {
 
     @BeforeEach
     public void setUp() {
+
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+
         //entity
         user = new User();
         user.setId(USER_ID);
         user.setUsername(USERNAME);
         user.setFirstName(FIRSTNAME);
         user.setLastName(LASTNAME);
-        user.setEmail(EMAIL_1);
+        user.setEmail(EMAIL);
         user.setPhone(PHONE);
 
         //dto
@@ -80,8 +91,16 @@ class UserControllerTest {
         userTo.setId(USER_ID);
         userTo.setFirstName(FIRSTNAME);
         userTo.setLastName(LASTNAME);
-        userTo.setEmail(EMAIL_1);
+        userTo.setEmail(EMAIL);
         userTo.setPhone(PHONE);
+
+        //Json
+        userObject = new JSONObject();
+        userObject.put("id", USER_ID);
+        userObject.put("firstName", FIRSTNAME);
+        userObject.put("lastName", LASTNAME);
+        userObject.put("email", EMAIL);
+        userObject.put("phone", PHONE_2);
     }
 
     @WithMockUser(username="admin", authorities = "ADMIN")
@@ -89,6 +108,7 @@ class UserControllerTest {
     void shouldGetCurrentUserByUsername() throws Exception {
         when(userRepository.findUsersByUsername(any())).thenReturn(Optional.ofNullable(user));
         when(auth.getName()).thenReturn(USERNAME);
+
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/users/me")
                 )
@@ -97,26 +117,30 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.username").value(USERNAME))
                 .andExpect(jsonPath("$.firstName").value(FIRSTNAME))
                 .andExpect(jsonPath("$.lastName").value(LASTNAME))
-                .andExpect(jsonPath("$.email").value(EMAIL_1))
+                .andExpect(jsonPath("$.email").value(EMAIL))
                 .andExpect(jsonPath("$.phone").value(PHONE));
     }
 
     @WithMockUser(username=USERNAME, authorities = "USER")
     @Test
     void shouldUpdateUser() throws Exception {
-//        user.setPhone(PHONE_2);
-//        when(userRepository.findById(any())).thenReturn(Optional.of(user));
-//        when(auth.getName()).thenReturn(USERNAME);
-//        mockMvc.perform(MockMvcRequestBuilders
-//                        .patch("/users/me")
-//                )
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").value(USER_ID))
-//                .andExpect(jsonPath("$.username").value(USERNAME))
-//                .andExpect(jsonPath("$.firstName").value(FIRSTNAME))
-//                .andExpect(jsonPath("$.lastName").value(LASTNAME))
-//                .andExpect(jsonPath("$.email").value(EMAIL_1))
-//                .andExpect(jsonPath("$.phone").value(PHONE_2));
+        when(auth.getName()).thenReturn(USERNAME);
+        when(userRepository.findById(any())).thenReturn(Optional.of(user));
+        user.setPhone(PHONE_2);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .patch("/users/me")
+                        .content(userObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(USER_ID))
+                .andExpect(jsonPath("$.firstName").value(FIRSTNAME))
+                .andExpect(jsonPath("$.lastName").value(LASTNAME))
+                .andExpect(jsonPath("$.email").value(EMAIL))
+                .andExpect(jsonPath("$.phone").value(PHONE_2));
     }
 
     @WithMockUser(username="admin", authorities = "ADMIN")
@@ -124,14 +148,15 @@ class UserControllerTest {
     void shouldGetAnyUserById() throws Exception {
         when(userRepository.findById(any())).thenReturn(Optional.ofNullable(user));
         when(userMapper.usersEntityToUserDto(any())).thenReturn(userTo);
+
         mockMvc.perform(MockMvcRequestBuilders
-                        .get("/users/1")
+                        .get("/users/" + USER_ID)
                 )
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(USER_ID))
                 .andExpect(jsonPath("$.firstName").value(FIRSTNAME))
                 .andExpect(jsonPath("$.lastName").value(LASTNAME))
-                .andExpect(jsonPath("$.email").value(EMAIL_1))
+                .andExpect(jsonPath("$.email").value(EMAIL))
                 .andExpect(jsonPath("$.phone").value(PHONE));
     }
 
@@ -139,15 +164,16 @@ class UserControllerTest {
     @Test
     void shouldGetAllUsersWithOrderById() throws Exception {
         when(userMapper.usersEntitiesToUserDtos(any())).thenReturn(List.of(userTo));
+
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/users")
                 )
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.count").value(1)) //заменить на константу
+                .andExpect(jsonPath("$.count").value(COUNT))
                 .andExpect(jsonPath("$.results[*].id").value(USER_ID))
                 .andExpect(jsonPath("$.results[*].firstName").value(FIRSTNAME))
                 .andExpect(jsonPath("$.results[*].lastName").value(LASTNAME))
-                .andExpect(jsonPath("$.results[*].email").value(EMAIL_1))
+                .andExpect(jsonPath("$.results[*].email").value(EMAIL))
                 .andExpect(jsonPath("$.results[*].phone").value(PHONE));
     }
 }
